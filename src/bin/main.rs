3e8550014fb91ba0b0928739a03322af4dce1dbb
Karel Peeters <karel.peeters.leuven@gmail.com>
2020-08-28 18:27:04 +0200

    Split main into separate binaries and remove unused dependencies.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,7 @@
-use std::io;
-use std::io::{Read, stdin, Write};
-use std::net::TcpListener;
 use std::time::Instant;
 
-use derive_more::From;
 use rand::{Rng, SeedableRng};
 use rand::rngs::SmallRng;
-use regex::Regex;
 
 use sttt::board::{Board, board_from_compact_string, board_to_compact_string, Coord};
 use sttt::bot_game;
@@ -15,54 +10,7 @@ use sttt::mcts::MCTSBot;
 use sttt::minimax::MiniMaxBot;
 
 fn main() {
-    // _console_game();
-    // _bot_game();
-    // _test_compact_string();
     _time_mcts()
-    // _bot_server(MCTSBot::new(100_000, SmallRng::from_entropy())).unwrap()
-}
-
-#[derive(Debug, From)]
-enum Error {
-    IO(std::io::Error),
-    Utf8(std::str::Utf8Error),
-}
-
-fn _bot_server<B: Bot>(mut bot: B) -> Result<(), Error> {
-    println!("Before bind");
-
-    let listener = TcpListener::bind("::1:1576")?;
-
-    println!("Waiting for connection");
-    for stream in listener.incoming() {
-        println!("Got stream");
-
-        let mut stream = stream?;
-
-        loop {
-            println!("Listening");
-            let mut buf = [0; 81];
-            stream.read_exact(&mut buf)?;
-
-            let string = std::str::from_utf8(&buf)?;
-            println!("Received board {:?}", string);
-
-            let board = board_from_compact_string(string);
-
-            let start = Instant::now();
-            let mv = bot.play(&board);
-            println!("Bot took {}s to find move", (Instant::now() - start).as_secs_f32());
-
-            println!("Replying move {:?}", mv);
-
-            let mv_int = mv.map(Coord::o).unwrap_or(100);
-            stream.write(&[mv_int])?;
-
-            println!("Reply done");
-        }
-    }
-
-    Ok(())
 }
 
 fn _time_mcts() {
@@ -126,79 +74,6 @@ fn _bot_game() {
     );
 
     println!("{:?}", res);
-}
-
-fn _console_game<B: Bot>(mut bot: B) {
-    let move_regex = Regex::new(r"^(?P<om>\d+)\s*(?:,\s*)?(?P<os>\d+)$").unwrap();
-
-    let mut history = Vec::new();
-    let mut board = Board::new();
-
-    println!("{}", board);
-
-    let mut line = String::new();
-
-    loop {
-        //Player move
-        'playerMove: loop {
-            print!("Play move: ");
-            io::stdout().flush().expect("Could not flush stdout");
-
-            line.clear();
-            stdin().read_line(&mut line).unwrap();
-            let line = line.trim();
-
-            if line == "u" {
-                board = match history.pop() {
-                    Some(board) => {
-                        println!("Undo");
-                        println!("{}", board);
-                        board
-                    }
-                    None => {
-                        println!("No history");
-                        board
-                    }
-                }
-            } else if let Some(m) = move_regex.captures(&line) {
-                let om: u8 = m["om"].parse().unwrap();
-                let os: u8 = m["os"].parse().unwrap();
-
-                if om <= 8 && os <= 8 {
-                    let mv = Coord::from_oo(om, os);
-                    if board.is_available_move(mv) {
-                        history.push(board.clone());
-                        board.play(mv);
-                        println!("{}", board);
-                        break 'playerMove;
-                    } else {
-                        eprintln!("Move not available")
-                    }
-                } else {
-                    eprintln!("Illegal value")
-                }
-            } else {
-                eprintln!("Invalid move format")
-            }
-        }
-
-        if board.is_done() {
-            println!("You won :)");
-            break;
-        }
-
-        //Bot move
-        let mv = bot.play(&board)
-            .expect("Bot should return move");
-
-        board.play(mv);
-        println!("{}", board);
-
-        if board.is_done() {
-            println!("You lost :(");
-            break;
-        }
-    }
 }
 
 #[allow(unused)]
