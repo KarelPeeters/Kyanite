73af18a1dee0f639613437ab174da2b9586921fb
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-05-19 02:49:30 +0200

    Only use windows crate as dependency when actually compiling for windows.

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    #[cfg(windows)]
     windows::build!(
         Windows::Win32::WindowsProgramming::PROCESS_CREATION_FLAGS,
         Windows::Win32::SystemServices::{GetCurrentProcess, SetPriorityClass},
