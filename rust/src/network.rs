f2b3f677e5e47988f800bc00660110301b030339
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-05-22 00:44:40 +0200

    Implement batching for neural network evaluations.

--- rust/src/network.rs
+++ rust/src/network.rs
@@ -1,8 +1,8 @@
 use std::path::Path;
 
-use sttt::board::{Board, Coord};
-use tch::{CModule, IValue, Tensor, Device, TchError};
 use itertools::Itertools;
+use sttt::board::{Board, Coord};
+use tch::{CModule, Device, IValue, TchError, Tensor};
 
 #[derive(Debug)]
 pub struct Network {
@@ -23,22 +23,32 @@ impl Network {
     }
 
     pub fn evaluate(&mut self, board: &Board) -> NetworkEvaluation {
-        let mask = Coord::all()
-            .map(|c| board.is_available_move(c) as u8 as f32)
-            .collect_vec();
-        let mask = Tensor::of_slice(&mask).view([1, 9, 9]);
+        let mut result = self.evaluate_all(&[board.clone()]);
+        assert_eq!(result.len(), 1);
+        result.pop().unwrap()
+    }
 
+    pub fn evaluate_all(&mut self, boards: &[Board]) -> Vec<NetworkEvaluation> {
+        let mut mask = Vec::new();
         let mut tiles = Vec::new();
-        tiles.extend(Coord::all().map(|c| (board.tile(c) == board.next_player) as u8 as f32));
-        tiles.extend(Coord::all().map(|c| (board.tile(c) == board.next_player.other()) as u8 as f32));
-        let tiles = Tensor::of_slice(&tiles).view([2, 9, 9]);
-
         let mut macros = Vec::new();
-        macros.extend((0..9).map(|om| (board.macr(om) == board.next_player) as u8 as f32));
-        macros.extend((0..9).map(|om| (board.macr(om) == board.next_player.other()) as u8 as f32));
-        let macros = Tensor::of_slice(&macros).view([2, 3, 3]);
 
-        let input = [IValue::Tensor(mask), IValue::Tensor(tiles), IValue::Tensor(macros)];
+        for board in boards {
+            mask.extend(Coord::all().map(|c| board.is_available_move(c) as u8 as f32));
+
+            tiles.extend(Coord::all().map(|c| (board.tile(c) == board.next_player) as u8 as f32));
+            tiles.extend(Coord::all().map(|c| (board.tile(c) == board.next_player.other()) as u8 as f32));
+
+            macros.extend((0..9).map(|om| (board.macr(om) == board.next_player) as u8 as f32));
+            macros.extend((0..9).map(|om| (board.macr(om) == board.next_player.other()) as u8 as f32));
+        }
+
+        let batch_size = boards.len() as i64;
+        let batch_mask = Tensor::of_slice(&mask).view([batch_size, 9, 9]);
+        let batch_tiles = Tensor::of_slice(&tiles).view([batch_size, 2, 9, 9]);
+        let batch_macros = Tensor::of_slice(&macros).view([batch_size, 2, 3, 3]);
+
+        let input = [IValue::Tensor(batch_mask), IValue::Tensor(batch_tiles), IValue::Tensor(batch_macros)];
         let result = self.model.forward_is(&input);
 
         let output = match result {
@@ -51,19 +61,26 @@ impl Network {
             err => {
                 err.expect("Failed to call model");
                 unreachable!();
-            },
+            }
         };
 
         assert_eq!(2, output.len(), "Return value count mismatch");
-        let value = unwrap_tensor_with_shape(&output[0], &[1]);
-        let policy = unwrap_tensor_with_shape(&output[1], &[1, 81]);
+        let batch_value = unwrap_tensor_with_shape(&output[0], &[batch_size]);
+        let batch_policy = unwrap_tensor_with_shape(&output[1], &[batch_size, 81]);
 
-        let value = Vec::<f32>::from(value)[0];
-        let mut policy = Vec::<f32>::from(policy);
+        let batch_value = Vec::<f32>::from(batch_value);
+        let mut batch_policy = Vec::<f32>::from(batch_policy);
 
-        mask_and_softmax(&mut policy, board);
+        boards.iter().enumerate().map(|(i, board)| {
+            let range = (81 * i)..(81 * (i + 1));
 
-        NetworkEvaluation { value, policy }
+            mask_and_softmax(&mut batch_policy[range.clone()], board);
+
+            NetworkEvaluation {
+                value: batch_value[i],
+                policy: batch_policy[range].to_vec(),
+            }
+        }).collect_vec()
     }
 }
 
