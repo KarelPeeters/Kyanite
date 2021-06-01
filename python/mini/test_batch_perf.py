c23c0cc758efd61afe81c4900e137c0b34463a3b
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-06-01 20:12:01 +0200

    More performance experiments, it seems like storing the input data more compactly doesn't really help.

--- python/mini/test_batch_perf.py
+++ python/mini/test_batch_perf.py
@@ -2,50 +2,80 @@ import time
 
 import torch
 from matplotlib import pyplot
+from torch import nn
 
 from models import GoogleModel
 from util import load_data
 
-data = load_data("../../data/esat/test_data.csv")
-model = GoogleModel(channels=64, block_count=5, value_size=64, res=True)
-model = torch.jit.script(model)
 
-ROUNDS = 10
+class ModelWrapper(nn.Module):
+    def __init__(self, inner):
+        super().__init__()
+        self.inner = inner
 
-batch_sizes = [1, 2, 3, 4, 5, 10, 20, 50, 80, 100, 1000, 4000]
-cpu_throughputs = torch.zeros(ROUNDS, len(batch_sizes))
-cuda_throughputs = torch.zeros(ROUNDS, len(batch_sizes))
-
-for device, throughputs in [("cpu", cpu_throughputs), ("cuda", cuda_throughputs)]:
-    data = data.to(device)
-
-    for batch_size_i, batch_size in enumerate(batch_sizes):
-        print(f"Trying batch size {batch_size} on {device}")
-
-        batch = data.pick_batch(torch.arange(batch_size))
-
-        model.eval()
-        model.to(device)
-
-        for round in range(ROUNDS):
-            start = time.perf_counter()
-
-            value, _ = model(batch.mask.to(device), batch.x_tiles.to(device), batch.x_macros.to(device))
-            # force synchronization
-            value.sum().item()
-
-            delta = time.perf_counter() - start
-
-            throughputs[round, batch_size_i] = batch_size / delta
+    def forward(self, mask, tiles, macros):
+        return self.inner(mask.float(), tiles.float(), macros.float())
 
 
-pyplot.plot(batch_sizes, cpu_throughputs.mean(dim=0), label="cpu")
-pyplot.plot(batch_sizes, cuda_throughputs.mean(dim=0), label="gpu")
-pyplot.legend()
-pyplot.xlabel("batch size")
-pyplot.ylabel("throughput eval/s")
-pyplot.xscale("log")
-pyplot.yscale("log")
+def main():
+    data = load_data("../../data/esat/test_data.csv")
 
-pyplot.savefig("../ignored/throughputs.png")
-pyplot.show()
+    ROUNDS = 20
+    DEVICES = ["cuda"]
+    BATCH_SIZES = [100, 200, 500, 1000]
+    PARALLEL_COUNT = 1
+
+    google = GoogleModel(channels=64, block_count=5, value_size=64, res=True)
+    DTYPES = [
+        (torch.int8, torch.jit.script(ModelWrapper(google))),
+        (torch.float16, torch.jit.script(ModelWrapper(google))),
+        (torch.float32, torch.jit.script(google)),
+    ]
+
+    throughputs = torch.zeros(len(DTYPES), len(DEVICES), len(BATCH_SIZES))
+
+    for ti, (dtype, model) in enumerate(DTYPES):
+        for di, device in enumerate(DEVICES):
+            data = data.to(device)
+            model.eval()
+            model.to(device)
+
+            for bi, batch_size in enumerate(BATCH_SIZES):
+                print(f"Trying batch size {batch_size} on {device} with type {dtype}")
+
+                totals = [torch.tensor(0)] * PARALLEL_COUNT
+
+                batch = data.pick_batch(torch.arange(batch_size))
+                mask = batch.mask.type(dtype)
+                x_tiles = batch.x_tiles.type(dtype)
+                x_macros = batch.x_macros.type(dtype)
+
+                start = time.perf_counter()
+
+                for _ in range(ROUNDS):
+                    value, _ = model(mask.to(device), x_tiles.to(device), x_macros.to(device))
+
+                    # force synchronization to prevent out of memory
+                    totals[:-1] = totals[1:]
+                    totals[-1] = value.sum()
+                    totals[0].item()
+
+                delta = time.perf_counter() - start
+                throughputs[ti, di, bi] = batch_size * ROUNDS / delta
+
+    for di, device in enumerate(DEVICES):
+        for ti, (dtype, _) in enumerate(DTYPES):
+            pyplot.plot(BATCH_SIZES, throughputs[ti, di, :], label=f"{device} {dtype}")
+
+    pyplot.legend()
+    pyplot.xlabel("batch size")
+    pyplot.ylabel("throughput eval/s")
+    pyplot.xscale("log")
+    pyplot.yscale("log")
+
+    pyplot.savefig("../ignored/throughputs.png")
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    main()
