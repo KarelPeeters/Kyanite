e247346f575ec4f3e0a8416e400e4cd5116030b9
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-07-03 23:16:16 +0200

    Reproduce pytorch/onnx output mismatch bug.

--- python/mini/root_eval.py
+++ python/mini/root_eval.py
@@ -1,14 +1,24 @@
+import onnxruntime
 import torch
 
-model = torch.jit.load("../../data/esat3/modest/model_7_epochs.pt", map_location="cpu")
+model = torch.jit.load("../../data/esat3/modest/model_13_epochs.pt", map_location="cpu")
 
 input = torch.zeros(1, 5, 9, 9)
 input[0, 0, :, :] = 1
 
+print("Pytorch:")
 wdl, policy = model(input)
 
 torch.set_printoptions(linewidth=200)
-print("WDL:")
 print(torch.nn.functional.softmax(wdl, dim=-1))
-print("Policy:")
 print(torch.nn.functional.softmax(policy.view(-1, 81), dim=-1).view(-1, 9, 9))
+
+print("ONNX:")
+sess_options = onnxruntime.SessionOptions()
+session = onnxruntime.InferenceSession("../../data/esat3/modest/model_13_epochs.onnx", sess_options=sess_options)
+
+outputs = session.run(None, {"input": input.numpy()})
+onnx_wdl = torch.tensor(outputs[0])
+onnx_policy = torch.tensor(outputs[1])
+print(torch.nn.functional.softmax(onnx_wdl, dim=-1))
+print(torch.nn.functional.softmax(onnx_policy.view(-1, 81), dim=-1).view(-1, 9, 9))
