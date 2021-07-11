29d85c846837dd1e915303a78055fb185c7bad7b
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-07-11 23:30:05 +0200

    Add surface area to ataxx heuristic.

--- src/heuristic/ataxx_heuristic.rs
+++ src/heuristic/ataxx_heuristic.rs
@@ -1,8 +1,35 @@
 use crate::ai::minimax::Heuristic;
 use crate::board::{Board, Outcome};
-use crate::games::ataxx::AtaxxBoard;
+use crate::games::ataxx::{AtaxxBoard, Tiles};
 
-pub struct AtaxxTileHeuristic;
+pub struct AtaxxTileHeuristic {
+    tile_factor: i32,
+    surface_factor: i32,
+}
+
+impl AtaxxTileHeuristic {
+    pub fn new(tile_factor: i32, surface_factor: i32) -> Self {
+        AtaxxTileHeuristic { tile_factor, surface_factor }
+    }
+}
+
+impl Default for AtaxxTileHeuristic {
+    fn default() -> Self {
+        AtaxxTileHeuristic {
+            tile_factor: 1,
+            surface_factor: 0,
+        }
+    }
+}
+
+impl AtaxxTileHeuristic {
+    fn player_score(&self, board: &AtaxxBoard, tiles: Tiles) -> i32 {
+        let tile_count = tiles.count() as i32;
+        let surface_area = (tiles.copy_targets() & board.free_tiles()).count() as i32;
+
+        self.tile_factor * tile_count + self.surface_factor * surface_area
+    }
+}
 
 impl Heuristic<AtaxxBoard> for AtaxxTileHeuristic {
     type V = i32;
@@ -19,7 +46,7 @@ impl Heuristic<AtaxxBoard> for AtaxxTileHeuristic {
             }
         } else {
             let (next, other) = board.tiles_pov();
-            (next.count() as i32) - (other.count() as i32)
+            self.player_score(board, next) - self.player_score(board, other)
         }
     }
 }
