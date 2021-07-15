3532f8f0dd3e41ab563a93fef0bcbc063549d0b7
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-07-15 21:45:57 +0200

    Implement more game_stats.

--- src/util/game_stats.rs
+++ src/util/game_stats.rs
@@ -3,6 +3,7 @@ use std::hash::Hash;
 
 use internal_iterator::InternalIterator;
 
+use crate::ai::Bot;
 use crate::board::Board;
 
 /// The number of legal positions reachable after `depth` moves, including duplicates.
@@ -13,6 +14,7 @@ pub fn perft<B: Board>(board: &B, depth: u32) -> u64 {
 }
 
 fn perft_recuse<B: Board + Hash>(map: &mut HashMap<(B, u32), u64>, board: B, depth: u32) -> u64 {
+    //TODO we can move the counter one level up and count moves there, instead of actually playing them
     if depth == 0 { return 1; }
     if board.is_done() { return 0; }
 
@@ -31,4 +33,31 @@ fn perft_recuse<B: Board + Hash>(map: &mut HashMap<(B, u32), u64>, board: B, dep
 
     map.insert(key, p);
     return p;
+}
+
+#[derive(Debug)]
+pub struct GameStats {
+    pub game_length: f32,
+    pub available_moves: f32,
+}
+
+/// Return `GameStats` estimated from `n` games starting from `board` played by `bot`.
+pub fn average_game_stats<B: Board>(start: &B, mut bot: impl Bot<B>, n: u64) -> GameStats {
+    let mut total_moves = 0;
+    let mut total_positions = 0;
+
+    for _ in 0..n {
+        let mut board = start.clone();
+        while !board.is_done() {
+            total_moves += board.available_moves().count();
+            total_positions += 1;
+
+            board.play(bot.select_move(&board));
+        }
+    }
+
+    GameStats {
+        game_length: total_positions as f32 / n as f32,
+        available_moves: total_moves as f32 / total_positions as f32,
+    }
 }
