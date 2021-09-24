a1696942bde514987809eff4a69db0c0733e02f6
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-09-24 18:24:23 +0200

    Change hdf5 converter to stream in batches instead of loading everything into memory at once.

--- python/lib/dataset.py
+++ python/lib/dataset.py
@@ -6,6 +6,7 @@ import numpy as np
 from torch.utils.data import Dataset
 
 from lib.games import Game
+from lib.growable_array import GrowableArray
 
 
 class GameDataFile:
@@ -94,29 +95,60 @@ def map_bin_gz_to_hdf5(game: Game, bin_path: str) -> str:
     if os.path.exists(temp_h5_path):
         os.remove(temp_h5_path)
 
-    data_bytes = gzip.open(bin_path, "rb").read()
-    positions = np.frombuffer(data_bytes, dtype=np.float32).reshape(-1, game.data_width)
+    # 128MB of data so we don't use too much ram but still do things in large batches
+    max_batch_size = int(128 * 1024 * 1024 // 4 // game.data_width)
+    print("Opening zip")
+    input_reader = gzip.open(bin_path, "rb")
 
     with h5py.File(temp_h5_path, "w") as f:
+        print("Creating dataset")
         f.create_dataset("game", data=game.name)
-        f.create_dataset(
+        positions = f.create_dataset(
             "positions",
-            data=positions, dtype=np.float32,
-            compression="gzip", compression_opts=4, chunks=(1, game.data_width),
+            shape=(0, game.data_width),
+            maxshape=(None, game.data_width),
+            chunks=(1, game.data_width),
+            compression="gzip", compression_opts=4,
+            dtype=np.float32
         )
 
-        # extra information, compute it once now so we can load it later without having to decompress everything
-        ids = positions[:, 0:2].astype(int)
+        # allocate enough space for the batch positions (which are floats, so *4)
+        batch_buffer = bytearray(max_batch_size * game.data_width * 4)
 
-        position_ids = ids[:, 1]
-        game_ids = ids[:, 0]
+        # keep basic stats around uncompressed for summary information at the end
+        ids_buffer = GrowableArray(2)
+
+        while True:
+            prev_count = len(positions)
+
+            batch_size_bytes = input_reader.readinto(batch_buffer)
+            batch_size = batch_size_bytes // (game.data_width * 4)
+            assert batch_size * (game.data_width * 4) == batch_size_bytes, "Unexpected length of file"
+
+            batch_np = np.frombuffer(batch_buffer, dtype=np.float32, count=batch_size * game.data_width) \
+                .reshape(batch_size, game.data_width)
+
+            positions.resize(prev_count + batch_size, axis=0)
+            positions[prev_count:, :] = batch_np
+
+            ids_buffer.extend(batch_np[:, 0:2])
+            curr_game_count = int(np.max(batch_np[:, 0]))
+            print(f"Finished mapping {prev_count + batch_size} positions, from {curr_game_count} games")
+            if batch_size_bytes < len(batch_buffer):
+                break
+
+        print("Writing other stuff")
+
+        # extra information, compute it once now so we can load it later without having to decompress everything
+        ids = ids_buffer.values.astype(int)
+        position_ids = ids[:, 1].astype(int)
+        game_ids = ids[:, 0].astype(int)
+
         f.create_dataset("game_ids", data=game_ids)
         f.create_dataset("position_ids", data=position_ids)
 
         f.create_dataset("position_count", data=len(positions))
         f.create_dataset("game_count", data=np.max(game_ids) + 1)
-
-        f.flush()
 
     os.rename(temp_h5_path, h5_path)
     return h5_path
--- python/lib/logger.py
+++ python/lib/logger.py
@@ -4,6 +4,8 @@ from typing import List, Optional, Tuple
 
 import numpy as np
 
+from lib.growable_array import GrowableArray
+
 
 @dataclass
 class FinishedLogData:
@@ -138,36 +140,3 @@ class Logger:
             assert key in self.batch_keys, f"Unexpected key {key}"
 
         self._curr_batch_data[key] = value
-
-
-class GrowableArray:
-    def __init__(self, width: int, initial_values=None):
-        self.width = width
-
-        if initial_values is None:
-            self._values = np.full((1, width), np.NaN)
-            self._next_i = 0
-        else:
-            assert initial_values.shape[1] == width
-            self._values = initial_values
-            self._next_i = len(initial_values)
-
-    def __len__(self):
-        return self._next_i
-
-    @property
-    def values(self):
-        return self._values[:self._next_i, :]
-
-    def append(self, values: np.array):
-        assert len(values) == self.width
-
-        # grow array if necessary
-        if self._next_i == len(self._values):
-            old_values = self._values
-            self._values = np.full((2 * len(old_values), self.width), np.NaN)
-            self._values[:len(old_values), :] = old_values
-
-        # actually append values
-        self._values[self._next_i, :] = values
-        self._next_i += 1
