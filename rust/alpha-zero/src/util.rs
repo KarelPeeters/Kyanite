6e1325b9d183a4bcce8f946310d84a2eaeea3bff
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-11-02 16:37:04 +0100

    Initial prototype of custom pgn parser.

--- rust/alpha-zero/src/util.rs
+++ rust/alpha-zero/src/util.rs
@@ -1,6 +1,7 @@
 use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
 use std::iter::Zip;
+use std::time::Instant;
 
 use itertools::zip;
 use rand::{Error, Rng, RngCore};
@@ -119,5 +120,48 @@ pub unsafe fn subslice_start<T>(slice: &[T], sub_slice: &[T]) -> Option<usize> {
         Some(sub_slice.as_ptr().offset_from(slice.as_ptr()) as usize)
     } else {
         None
+    }
+}
+
+#[derive(Debug)]
+pub struct PrintThroughput {
+    name: String,
+    total_count: u64,
+    delta_count: u64,
+    update_count: u64,
+    last_print: Instant,
+}
+
+impl PrintThroughput {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_owned(),
+            total_count: 0,
+            delta_count: 0,
+            update_count: 0,
+            last_print: Instant::now(),
+        }
+    }
+
+    pub fn total_count(&self) -> u64 {
+        self.total_count
+    }
+
+    pub fn update(&mut self, count: u64) {
+        self.delta_count += count;
+        self.total_count += count;
+        self.update_count += 1;
+
+        let now = Instant::now();
+        let delta = now - self.last_print;
+
+        if delta.as_secs() >= 1 && self.update_count >= 10 {
+            let throughput = self.delta_count as f32 / delta.as_secs_f32();
+            println!("{:.3} {}/s => {:.3} {}", throughput, self.name, self.total_count, self.name);
+
+            self.last_print = now;
+            self.delta_count = 0;
+            self.update_count = 0;
+        }
     }
 }
---
+++ rust/pgn-reader/src/lib.rs
@@ -0,0 +1,130 @@
+use std::cmp::max;
+
+use buffered_reader::BufferedReader;
+use memchr::memchr;
+
+#[derive(Debug)]
+pub struct PgnReader<R> {
+    start_index: usize,
+    prev_game_length: usize,
+    input: R,
+}
+
+#[derive(Debug)]
+pub struct Game<'a> {
+    start_index: usize,
+    header: &'a str,
+    moves: &'a str,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidPgn(usize),
+    UnexpectedEof,
+    Io(std::io::Error),
+    Utf8(std::str::Utf8Error),
+}
+
+impl<R> PgnReader<R> {
+    pub fn new(input: R) -> Self {
+        Self {
+            start_index: 0,
+            prev_game_length: 0,
+            input,
+        }
+    }
+}
+
+impl<R: BufferedReader<()>> PgnReader<R> {
+    pub fn next(&mut self) -> Result<Option<Game>, Error> {
+        self.input.consume(self.prev_game_length);
+        if self.input.eof() { return Ok(None); }
+
+        // we're now at the start of a new game
+        let mut in_header = true;
+
+        let mut header_length = 0;
+        let mut moves_length = 0;
+
+        loop {
+            let start = header_length + moves_length;
+            let line = buffered_read_to_from(&mut self.input, start, b'\n')?;
+
+            match line.last() {
+                // reached eof, check if we still have a game after this loop
+                None => break,
+                // great, we've parsed a full line
+                Some(b'\n') => (),
+                // we ran into eof before a newline
+                Some(_) => return Err(Error::UnexpectedEof),
+            }
+
+            if line[0] == b'[' {
+                if !in_header {
+                    // the next game is starting, handle this next time
+                    break;
+                }
+                header_length += line.len();
+            } else {
+                in_header = false;
+                moves_length += line.len();
+            }
+        }
+
+        match (header_length, moves_length) {
+            (0, 0) =>
+                Ok(None),
+            (0, _) | (_, 0) =>
+                Err(Error::InvalidPgn(self.start_index)),
+            (_, _) => {
+                let data = self.input.buffer();
+                let total_length = header_length + moves_length;
+                assert!(data.len() >= total_length);
+
+                let header = std::str::from_utf8(&data[0..header_length])?;
+                let moves = std::str::from_utf8(&data[header_length..total_length])?;
+
+                let game = Game { start_index: self.start_index, header, moves };
+                self.start_index += total_length;
+                self.prev_game_length = total_length;
+
+                Ok(Some(game))
+            }
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error::Io(e)
+    }
+}
+
+impl From<std::str::Utf8Error> for Error {
+    fn from(e: std::str::Utf8Error) -> Self {
+        Error::Utf8(e)
+    }
+}
+
+/// Version of [BufferedReader::read_to] that only starts searching from the given index.
+/// Implementation based on the linked function.
+fn buffered_read_to_from(input: &mut impl BufferedReader<()>, start: usize, terminal: u8) -> Result<&[u8], std::io::Error> {
+    let mut n = 128;
+
+    let len = loop {
+        let data = &input.data(start + n)?[start..];
+
+        if let Some(newline) = memchr(terminal, data)
+        {
+            break newline + 1;
+        } else if data.len() < n {
+            // EOF.
+            break data.len();
+        } else {
+            // Read more data.
+            n = max(2 * n, data.len() + 1024);
+        }
+    };
+
+    Ok(&input.buffer()[start..start + len])
+}
