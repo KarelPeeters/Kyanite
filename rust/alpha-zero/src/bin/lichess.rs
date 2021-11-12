954485a242b5f21abbd86f819b8e4d6d12a568ca
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-11-12 23:56:14 +0100

    Lichess client cleanup with attempt to read chat, without success so far.

--- rust/alpha-zero/src/bin/lichess.rs
+++ rust/alpha-zero/src/bin/lichess.rs
@@ -1,18 +1,23 @@
+use std::collections::HashSet;
 use std::time::{Duration, Instant};
 
 use board_game::board::Board;
 use board_game::games::chess::ChessBoard;
 use itertools::Itertools;
 use tokio_stream::StreamExt;
-use unwrap_match::unwrap_match;
 
 use alpha_zero::mapping::chess::ChessStdMapper;
 use alpha_zero::network::cudnn::CudnnNetwork;
+use alpha_zero::network::Network;
 use alpha_zero::zero::wrapper::ZeroSettings;
 use cuda_nn_eval::Device;
 use licoricedev::client::{Lichess, LichessResult};
 use licoricedev::models::board::{BoardState, GameFull};
+use licoricedev::models::game::UserGame;
 use nn_graph::onnx::load_graph_from_onnx_path;
+
+const MAX_VISITS: u64 = 100_000;
+const MAX_FRACTION_TIME_USED: f32 = 1.0 / 30.0;
 
 fn main() {
     tokio::runtime::Builder::new_current_thread()
@@ -33,9 +38,6 @@ async fn main_async() {
 }
 
 async fn main_inner() -> LichessResult<()> {
-    let max_visits = 100_000;
-    let max_fraction_time_used = 1.0 / 30.0;
-
     let path = std::fs::read_to_string("ignored/network_path.txt").unwrap();
     let graph = load_graph_from_onnx_path(path);
     let settings = ZeroSettings::new(128, 4.0, false);
@@ -43,6 +45,8 @@ async fn main_inner() -> LichessResult<()> {
 
     let token = std::fs::read_to_string("ignored/lichess_token.txt")?;
     let lichess = Lichess::new(token);
+
+    let mut info_game_ids: HashSet<String> = Default::default();
 
     loop {
         let mut was_my_turn = false;
@@ -56,52 +60,85 @@ async fn main_inner() -> LichessResult<()> {
             }
             was_my_turn = true;
 
-            let mut state = lichess.stream_bot_game_state(&game.game_id).await?;
-            if let Some(state) = state.next().await {
+            let mut state_stream = lichess.stream_bot_game_state(&game.game_id).await?;
+            if let Some(state) = state_stream.next().await.transpose()? {
                 println!("{:?}", state);
-                let state: GameFull = unwrap_match!(state?, BoardState::GameFull(state) => state);
-                let board = board_from_moves(&state.state.moves);
-                println!("{}", board);
 
-                let start = Instant::now();
-                let tree = settings.build_tree(&board, &mut network, |tree| {
-                    println!("{}, {:.2?}", tree.root_visits(), tree.values().wdl);
-                    let fraction_time_used = (Instant::now() - start).as_secs_f32() / game.seconds_left as f32;
-                    let visits = tree.root_visits();
-                    visits > 0 && (visits >= max_visits || fraction_time_used >= max_fraction_time_used)
-                });
-
-                println!("{}", tree.display(3, true, 5));
-                let mv = tree.best_move();
-
-                if let Err(e) = lichess.make_a_bot_move(&game.game_id, &mv.to_string(), false).await {
-                    // can happen when the other player resigns or aborts the game
-                    println!("Error while playing move: {:?}", e);
+                match state {
+                    BoardState::GameState(state) => {
+                        println!("Received partial state {:?}", state);
+                    }
+                    BoardState::ChatLine(line) => {
+                        match &*line.text {
+                            "info start" => {
+                                info_game_ids.insert(game.game_id.clone());
+                            }
+                            "info stop" => {
+                                info_game_ids.remove(&game.game_id);
+                            }
+                            _ => {}
+                        }
+                    }
+                    BoardState::GameFull(state) => {
+                        let print = info_game_ids.contains(&state.id);
+                        make_move(&lichess, &game, &state, print, settings, &mut network).await?;
+                    }
                 }
-
-                let pv = tree.principal_variation(3).iter().skip(1).join(" ");
-
-                let message = format!(
-                    "visits: {}, depth: {:?}, pv: {}",
-                    tree.root_visits(), tree.depth_range(0), pv,
-                );
-                println!("Sending {:?}", message);
-                lichess.write_in_bot_chat(&game.game_id, "player", &message).await?;
-
-                let message = format!(
-                    "zero: {:.2?}, net: {:.2?}",
-                    tree.values().wdl.to_slice(), tree[0].net_values.unwrap().wdl.to_slice(),
-                );
-                println!("Sending {:?}", message);
-                lichess.write_in_bot_chat(&game.game_id, "player", &message).await?;
             }
         }
 
         if !was_my_turn {
             // wait for a bit
-            std::thread::sleep(Duration::from_secs_f32(1.0));
+            std::thread::sleep(Duration::from_secs(1));
         }
     }
+}
+
+async fn make_move(
+    lichess: &Lichess,
+    game: &UserGame,
+    state: &GameFull,
+    info: bool,
+    settings: ZeroSettings,
+    network: &mut impl Network<ChessBoard>,
+) -> LichessResult<()> {
+    let board = board_from_moves(&state.state.moves);
+    println!("{}", board);
+
+    let start = Instant::now();
+    let tree = settings.build_tree(&board, network, |tree| {
+        let fraction_time_used = (Instant::now() - start).as_secs_f32() / game.seconds_left as f32;
+        let visits = tree.root_visits();
+        visits > 0 && (visits >= MAX_VISITS || fraction_time_used >= MAX_FRACTION_TIME_USED)
+    });
+
+    println!("{}", tree.display(3, true, 5));
+    let mv = tree.best_move();
+
+    if let Err(e) = lichess.make_a_bot_move(&game.game_id, &mv.to_string(), false).await {
+        // can happen when the other player resigns or aborts the game
+        println!("Error while playing move: {:?}", e);
+    }
+
+    if info {
+        let pv = tree.principal_variation(3).iter().skip(1).join(" ");
+
+        let message = format!(
+            "visits: {}, depth: {:?}, pv: {}",
+            tree.root_visits(), tree.depth_range(0), pv,
+        );
+        println!("Sending {:?}", message);
+        lichess.write_in_bot_chat(&game.game_id, "player", &message).await?;
+
+        let message = format!(
+            "zero: {:.2?}, net: {:.2?}",
+            tree.values().wdl.to_slice(), tree[0].net_values.unwrap().wdl.to_slice(),
+        );
+        println!("Sending {:?}", message);
+        lichess.write_in_bot_chat(&game.game_id, "player", &message).await?;
+    }
+
+    Ok(())
 }
 
 fn board_from_moves(moves: &str) -> ChessBoard {
