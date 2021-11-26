06b9bcf0f93bcfc33ff8d0245b6d95d146163a1e
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-11-26 17:34:42 +0100

    Update pgn-reader tests.

--- rust/pgn-reader/tests/basic.rs
+++ rust/pgn-reader/tests/basic.rs
@@ -1,6 +1,7 @@
 use buffered_reader::Memory;
+use internal_iterator::InternalIterator;
 
-use pgn_reader::{PgnReader, PgnResult};
+use pgn_reader::{PgnMove, PgnReader, PgnResult};
 
 fn read(input: &str) -> PgnReader<Memory<()>> {
     let read = Memory::new(input.as_bytes());
@@ -18,7 +19,8 @@ fn short() {
 
     let game = reader.next().unwrap().unwrap();
     assert_eq!(game.header("Test"), Some("test"));
-    assert_eq!(game.parse_moves(), (vec!["e4"], PgnResult::Draw));
+    assert_eq!(game.move_iter().collect::<Vec<_>>(), vec![PgnMove { mv: "e4", comment: None }]);
+    assert_eq!(game.result(), PgnResult::Draw);
 
     assert_eq!(reader.next().unwrap(), None);
 }
@@ -29,7 +31,8 @@ fn multi_digit() {
 
     let game = reader.next().unwrap().unwrap();
     assert_eq!(game.header("Test"), Some("test"));
-    assert_eq!(game.parse_moves(), (vec!["e4"], PgnResult::Draw));
+    assert_eq!(game.move_iter().collect::<Vec<_>>(), vec![PgnMove { mv: "e4", comment: None }]);
+    assert_eq!(game.result(), PgnResult::Draw);
 
     assert_eq!(reader.next().unwrap(), None);
 }
@@ -40,18 +43,47 @@ fn multiple_moves_per_turn() {
 
     let game = reader.next().unwrap().unwrap();
     assert_eq!(game.header("Test"), Some("test"));
-    assert_eq!(game.parse_moves(), (vec!["e4", "e5", "d4", "d5"], PgnResult::WinWhite));
+    assert_eq!(
+        game.move_iter().collect::<Vec<_>>(),
+        vec![
+            PgnMove { mv: "e4", comment: None },
+            PgnMove { mv: "e5", comment: None },
+            PgnMove { mv: "d4", comment: None },
+            PgnMove { mv: "d5", comment: None },
+        ]
+    );
+    assert_eq!(game.result(), PgnResult::WinWhite);
 
     assert_eq!(reader.next().unwrap(), None);
 }
 
 #[test]
 fn variation() {
-    let mut reader = read("[Test \"test\"]\n1. e4 { d5 6d d8 } 1... d5 { [%clk 0:02:55] } { fsdf {  dsf  } { 0-1 } } a6 1-0\n");
+    let mut reader = read("[Test \"test\"]\n1. e4 { d5 6d d8 } 1... d5 { [%clk 0:02:55] } { foo {  dsf  } { 0-1 } } a6 1-0\n");
 
     let game = reader.next().unwrap().unwrap();
     assert_eq!(game.header("Test"), Some("test"));
-    assert_eq!(game.parse_moves(), (vec!["e4", "d5", "a6"], PgnResult::WinWhite));
+    assert_eq!(
+        game.move_iter().collect::<Vec<_>>(),
+        vec![
+            PgnMove { mv: "e4", comment: Some("d5 6d d8") },
+            PgnMove { mv: "d5", comment: Some("[%clk 0:02:55]") },
+            PgnMove { mv: "a6", comment: None },
+        ]
+    );
+    assert_eq!(game.result(), PgnResult::WinWhite);
 
     assert_eq!(reader.next().unwrap(), None);
+}
+
+#[test]
+fn comments() {
+    let mv = PgnMove {
+        mv: "e4",
+        comment: Some("[%eval 0.16] [%clk 0:00:30]"),
+    };
+
+    assert_eq!(mv.field("eval"), Some("0.16"));
+    assert_eq!(mv.field("clk"), Some("0:00:30"));
+    assert_eq!(mv.field("derp"), None);
 }
