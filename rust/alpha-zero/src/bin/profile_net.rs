be7440edf4fe7874a88f141f3dd5b9e52fb50ca4
Karel Peeters <karel.peeters.leuven@gmail.com>
2021-12-18 14:14:20 +0100

    Add option to profile_net to set number of iterations.

--- rust/alpha-zero/src/bin/profile_net.rs
+++ rust/alpha-zero/src/bin/profile_net.rs
@@ -21,16 +21,20 @@ struct Args {
     #[clap(short, long)]
     print: bool,
 
+    #[clap(long)]
+    n: Option<usize>,
+
     path: String,
 }
 
 const TEST_BATCH_SIZES: &[usize] = &[1, 2, 3, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
 const TEST_BATCH_ITERATIONS: &[usize] = &[100, 100, 100, 100, 100, 100, 100, 100, 50, 25, 12, 6, 3];
 
-const ITERATIONS: usize = 100;
+const DEFAULT_ITERATIONS: usize = 100;
 
 fn main() {
-    let Args { batch_size, optimize, graph: use_graph, print, path } = Args::parse();
+    let Args { batch_size, optimize, graph: use_graph, print, n, path } = Args::parse();
+    let n = n.unwrap_or(DEFAULT_ITERATIONS);
 
     if cfg!(debug_assertions) {
         println!("Warning: debug assertions are enabled, maybe this binary is not optimized either?");
@@ -58,7 +62,7 @@ fn main() {
     if batch_size < 1 {
         profile_different_batch_sizes(&graph, use_graph);
     } else {
-        profile_single_batch_size(&graph, use_graph, batch_size as usize);
+        profile_single_batch_size(&graph, use_graph, batch_size as usize, n);
     }
 }
 
@@ -90,24 +94,24 @@ fn profile_different_batch_sizes(graph: &Graph, use_graph: bool) {
     println!("{:?}", result);
 }
 
-fn profile_single_batch_size(graph: &Graph, use_graph: bool, batch_size: usize) {
+fn profile_single_batch_size(graph: &Graph, use_graph: bool, batch_size: usize, n: usize) {
     let mut executor = CudnnExecutor::new(Device::new(0), &graph, batch_size, use_graph);
 
     let inputs = dummy_inputs(&graph, batch_size);
     let inputs = inputs.iter().map(|v| &**v).collect_vec();
 
     println!("Warmup");
-    for _ in 0..max(1, ITERATIONS / 10) {
+    for _ in 0..max(1, n / 10) {
         executor.evaluate(&inputs);
     }
 
     println!("Throughput test");
     let start = Instant::now();
-    for _ in 0..ITERATIONS {
+    for _ in 0..n {
         executor.evaluate(&inputs);
     }
     let delta = (Instant::now() - start).as_secs_f32();
-    let throughput = (batch_size * ITERATIONS) as f32 / delta;
+    let throughput = (batch_size * n) as f32 / delta;
 
     println!("Profiling");
     executor.set_profile(true);
