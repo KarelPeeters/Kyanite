bc9be5fd51ab858711af8a179f141c51f0b4d48b
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-04-12 12:13:37 +0200

    Add multithreading to selfuni.

--- rust/kz-selfplay/src/bin/selfuni.rs
+++ rust/kz-selfplay/src/bin/selfuni.rs
@@ -1,3 +1,6 @@
+use std::borrow::Cow;
+use std::path::PathBuf;
+
 use board_game::ai::solver::solve_all_moves;
 use board_game::board::{Board, Outcome};
 use board_game::games::ataxx::AtaxxBoard;
@@ -6,35 +9,38 @@ use board_game::games::sttt::STTTBoard;
 use board_game::games::ttt::TTTBoard;
 use board_game::wdl::OutcomeWDL;
 use clap::Parser;
+use crossbeam::channel;
+use crossbeam::channel::{Receiver, Sender};
+use internal_iterator::InternalIterator;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 use kz_core::mapping::ataxx::AtaxxStdMapper;
 use kz_core::mapping::chess::ChessStdMapper;
 use kz_core::mapping::sttt::STTTStdMapper;
 use kz_core::mapping::ttt::TTTStdMapper;
 use kz_core::mapping::BoardMapper;
-use kz_selfplay::binary_output::BinaryOutput;
-use kz_selfplay::server::protocol::Game;
-use std::borrow::Cow;
-use std::path::PathBuf;
-
-use internal_iterator::InternalIterator;
 use kz_core::network::dummy::{uniform_policy, uniform_values};
 use kz_core::network::ZeroEvaluation;
 use kz_core::zero::node::ZeroValues;
+use kz_selfplay::binary_output::BinaryOutput;
+use kz_selfplay::server::protocol::Game;
 use kz_selfplay::simulation::{Position, Simulation};
 use kz_util::PrintThroughput;
-use rand::seq::SliceRandom;
-use rand::thread_rng;
 
 #[derive(Debug, Parser)]
 struct Args {
     #[clap(long)]
     game: String,
     #[clap(long)]
-    max_game_length: u64,
-    #[clap(long)]
     game_count: u64,
     #[clap(long)]
-    solver_depth: u32,
+    max_game_length: u64,
+
+    #[clap(long)]
+    solver_depth: Option<u32>,
+    #[clap(long)]
+    thread_count: Option<u32>,
 
     bin_path: PathBuf,
 }
@@ -45,31 +51,60 @@ fn main() {
     let game = Game::parse(&args.game).unwrap();
 
     match game {
-        Game::TTT => main_impl(TTTBoard::default, TTTStdMapper, args),
-        Game::STTT => main_impl(STTTBoard::default, STTTStdMapper, args),
-        Game::Chess => main_impl(ChessBoard::default, ChessStdMapper, args),
-        Game::Ataxx { size } => main_impl(|| AtaxxBoard::diagonal(size), AtaxxStdMapper::new(size), args),
+        Game::TTT => main_impl(&args, TTTBoard::default, TTTStdMapper),
+        Game::STTT => main_impl(&args, STTTBoard::default, STTTStdMapper),
+        Game::Chess => main_impl(&args, ChessBoard::default, ChessStdMapper),
+        Game::Ataxx { size } => main_impl(&args, || AtaxxBoard::diagonal(size), AtaxxStdMapper::new(size)),
     }
 }
 
-fn main_impl<B: Board, M: BoardMapper<B>>(mut start_pos: impl FnMut() -> B, mapper: M, args: Args) {
-    let Args {
-        game,
-        max_game_length,
-        game_count,
-        bin_path,
-        solver_depth,
-    } = args;
+fn main_impl<B: Board, M: BoardMapper<B>>(args: &Args, start_pos: impl Fn() -> B + Sync, mapper: M) {
+    let thread_count = args.thread_count.unwrap_or(1) as usize;
 
-    let mut rng = thread_rng();
-    let mut output = BinaryOutput::new(bin_path, &game, mapper).unwrap();
+    crossbeam::scope(|s| {
+        let (sender, receiver) = channel::bounded(2 * thread_count);
+
+        // spawn generators
+        for i in 0..thread_count {
+            let sender = sender.clone();
+
+            s.builder()
+                .name(format!("generator-{}", i))
+                .spawn(|_| {
+                    main_generator(args, &start_pos, sender);
+                })
+                .unwrap();
+        }
+
+        // run collection in main thread
+        main_collector(args, mapper, receiver);
+    })
+    .unwrap();
+}
+
+fn main_collector<B: Board, M: BoardMapper<B>>(args: &Args, mapper: M, receiver: Receiver<Simulation<B>>) {
+    let mut output = BinaryOutput::new(&args.bin_path, &args.game, mapper).unwrap();
     let mut tp = PrintThroughput::new("games");
 
-    for _ in 0..game_count {
+    for _ in 0..args.game_count {
+        let sim = receiver.recv().unwrap();
+        output.append(&sim).unwrap();
+        tp.update_delta(1);
+    }
+
+    output.finish().unwrap();
+    std::mem::drop(receiver);
+}
+
+fn main_generator<B: Board>(args: &Args, start_pos: impl Fn() -> B, sender: Sender<Simulation<B>>) {
+    let mut rng = thread_rng();
+    let solver_depth = args.solver_depth.unwrap_or(0);
+
+    loop {
         let mut board = start_pos();
         let mut positions = vec![];
 
-        for _ in 0..max_game_length {
+        for _ in 0..args.max_game_length {
             if board.is_done() {
                 break;
             }
@@ -115,9 +150,9 @@ fn main_impl<B: Board, M: BoardMapper<B>>(mut start_pos: impl FnMut() -> B, mapp
         let outcome = board.outcome().unwrap_or(Outcome::Draw);
         let sim = Simulation { outcome, positions };
 
-        output.append(&sim).unwrap();
-        tp.update_delta(1);
+        match sender.send(sim) {
+            Ok(()) => {}
+            Err(_) => break,
+        }
     }
-
-    output.finish().unwrap();
 }
