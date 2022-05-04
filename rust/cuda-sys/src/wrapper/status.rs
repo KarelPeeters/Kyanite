42d88df13a52adb7c9a81e4c38810f8bb19285b6
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-05-04 17:43:38 +0200

    Add Status::is_success to more easily write custom panic messages.

--- rust/cuda-sys/src/wrapper/status.rs
+++ rust/cuda-sys/src/wrapper/status.rs
@@ -6,14 +6,25 @@ use crate::bindings::{
 };
 use crate::bindings::{cudnnGetErrorString, cudnnStatus_t};
 
-pub trait Status {
+pub trait Status: Copy + PartialEq {
+    const SUCCESS: Self;
+
     fn as_string(&self) -> &'static str;
 
+    fn is_success(&self) -> bool {
+        *self == Self::SUCCESS
+    }
+
     #[track_caller]
-    fn unwrap(&self);
+    fn unwrap(&self) {
+        if !self.is_success() {
+            panic!("Operation returned error {:?}", self.as_string());
+        }
+    }
 
     /// Alternative to `unwrap` that only panics if `!std::thread::panicking()`.
     /// This is useful to avoid double panics in [Drop] implementations.
+    #[track_caller]
     fn unwrap_in_drop(&self) {
         if !std::thread::panicking() {
             self.unwrap();
@@ -22,30 +33,24 @@ pub trait Status {
 }
 
 impl Status for cudaError {
+    const SUCCESS: Self = cudaError::cudaSuccess;
+
     fn as_string(&self) -> &'static str {
         unsafe { CStr::from_ptr(cudaGetErrorString(*self)) }.to_str().unwrap()
-    }
-
-    fn unwrap(&self) {
-        if *self != cudaError::cudaSuccess {
-            panic!("Cuda operation returned error {:?}", self.as_string());
-        }
     }
 }
 
 impl Status for cudnnStatus_t {
+    const SUCCESS: Self = cudnnStatus_t::CUDNN_STATUS_SUCCESS;
+
     fn as_string(&self) -> &'static str {
         unsafe { CStr::from_ptr(cudnnGetErrorString(*self)) }.to_str().unwrap()
-    }
-
-    fn unwrap(&self) {
-        if *self != cudnnStatus_t::CUDNN_STATUS_SUCCESS {
-            panic!("Cudnn operation returned error {:?}", self);
-        }
     }
 }
 
 impl Status for cublasStatus_t {
+    const SUCCESS: Self = cublasStatus_t::CUBLAS_STATUS_SUCCESS;
+
     fn as_string(&self) -> &'static str {
         match self {
             cublasStatus_t::CUBLAS_STATUS_SUCCESS => "CUBLAS_STATUS_SUCCESS",
@@ -60,27 +65,19 @@ impl Status for cublasStatus_t {
             cublasStatus_t::CUBLAS_STATUS_LICENSE_ERROR => "CUBLAS_STATUS_LICENSE_ERROR",
         }
     }
-
-    fn unwrap(&self) {
-        if *self != cublasStatus_t::CUBLAS_STATUS_SUCCESS {
-            panic!("Cublas operation returned error {:?}", self.as_string());
-        }
-    }
 }
 
 impl Status for nvrtcResult {
+    const SUCCESS: Self = nvrtcResult::NVRTC_SUCCESS;
+
     fn as_string(&self) -> &'static str {
         unsafe { CStr::from_ptr(nvrtcGetErrorString(*self)) }.to_str().unwrap()
-    }
-
-    fn unwrap(&self) {
-        if *self != nvrtcResult::NVRTC_SUCCESS {
-            panic!("NVRTC operation returned error {:?}", self.as_string())
-        }
     }
 }
 
 impl Status for CUresult {
+    const SUCCESS: Self = CUresult::CUDA_SUCCESS;
+
     fn as_string(&self) -> &'static str {
         unsafe {
             let mut ptr = null();
@@ -89,12 +86,6 @@ impl Status for CUresult {
                 panic!("Error '{:?}' while getting name of error '{:?}'", result, self);
             }
             CStr::from_ptr(ptr).to_str().unwrap()
-        }
-    }
-
-    fn unwrap(&self) {
-        if *self != CUresult::CUDA_SUCCESS {
-            panic!("CU driver operation returned error {:?}", self.as_string())
         }
     }
 }
