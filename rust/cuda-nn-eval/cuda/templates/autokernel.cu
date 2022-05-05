e8c8d25d75f4db7d6c8735c0fb4c1a0a80754d05
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-05-05 16:42:18 +0200

    Compare autokernel to cudnn kernel, and add op parameter that seems to have no runtime cost.

--- rust/cuda-nn-eval/cuda/templates/autokernel.cu
+++ rust/cuda-nn-eval/cuda/templates/autokernel.cu
@@ -1,14 +1,14 @@
-
 __host__ __device__ int ceil_div(int x, int y) {
     return (x + y - 1) / y;
 }
 
 extern "C" __global__ void
 foo_kernel(
+        int op,
         int shape_0, int shape_1,
-        int stride0_0, int stride0_1, float *f0,
-        int stride1_0, int stride1_1, float *f1,
-        int stride2_0, int stride2_1, float *f2
+        int stride0_0, int stride0_1, float *ptr0,
+        int stride1_0, int stride1_1, float *ptr1,
+        int stride2_0, int stride2_1, float *ptr2
 ) {
     int blockCount = gridDim.x;
     int threadsPerBlock = blockDim.x;
@@ -20,7 +20,6 @@ foo_kernel(
 
     int size = shape_0 * shape_1;
     int itemsPerThread = ceil_div(size, threadCount);
-    int start_index = global * itemsPerThread;
 
     for (int index = global; index < size; index += threadCount) {
         int a0 = index / shape_1;
@@ -30,6 +29,10 @@ foo_kernel(
         int i1 = a0 * stride1_0 + a1 * stride1_1;
         int i2 = a0 * stride2_0 + a1 * stride2_1;
 
-        f2[i2] = f0[i0] + f1[i1];
+        if (op) {
+            ptr2[i2] = ptr2[i0] + ptr1[i1];
+        } else {
+            ptr2[i2] = ptr2[i0];
+        }
     }
 }
--- rust/cuda-nn-eval/src/bin/autokernel.rs
+++ rust/cuda-nn-eval/src/bin/autokernel.rs
@@ -1,13 +1,11 @@
 extern crate core;
 
-use itertools::Itertools;
-
 use cuda_nn_eval::device_tensor::DeviceTensor;
-use cuda_nn_eval::shape::StridedShape;
-use cuda_sys::bindings::{cudaError, cudaStream_t, cudnnOpTensorOp_t};
+use cuda_sys::bindings::cudnnOpTensorOp_t;
 use cuda_sys::wrapper::descriptor::TensorOpDescriptor;
 use cuda_sys::wrapper::group::TensorOpArgs;
 use cuda_sys::wrapper::handle::{CudaStream, CudnnHandle, Device};
+use cuda_sys::wrapper::mem::device::DevicePtr;
 use cuda_sys::wrapper::rtc::args::KernelArgs;
 use cuda_sys::wrapper::rtc::core::{CuFunction, CuModule, Dim3};
 use cuda_sys::wrapper::status::Status;
@@ -32,6 +30,8 @@ unsafe fn launch(stream: &CudaStream, func: &CuFunction, a: &DeviceTensor, b: &D
     let args = {
         let mut args = KernelArgs::new();
 
+        args.push_int(0 as i32);
+
         args.push_int(shape[0] as i32);
         args.push_int(shape[1] as i32);
 
@@ -52,6 +52,20 @@ unsafe fn launch(stream: &CudaStream, func: &CuFunction, a: &DeviceTensor, b: &D
     func.launch_kernel(Dim3::single(blocks), Dim3::single(threads_per_block), 0, stream, &args);
 }
 
+unsafe fn profile_kernel(stream: &CudaStream, f: impl Fn()) -> f32 {
+    let iterations = 100;
+
+    let start = stream.record_new_event();
+    for _ in 0..iterations {
+        f();
+    }
+    let end = stream.record_new_event();
+    stream.synchronize();
+
+    let time_per_kernel = end.time_elapsed_since(&start) / iterations as f32;
+    time_per_kernel
+}
+
 unsafe fn main_inner() {
     let template_path = "cuda-nn-eval/cuda/templates/autokernel.cu";
     let template = std::fs::read_to_string(template_path).unwrap();
@@ -67,31 +81,49 @@ unsafe fn main_inner() {
     let func = module.get_function("foo_kernel").unwrap();
 
     let a_inner = DeviceTensor::alloc_simple(device, vec![1024, 256 * 8 * 8]);
-    let b_inner = DeviceTensor::alloc_simple(device, vec![1, 1]);
+    let b_inner = DeviceTensor::alloc_simple(device, vec![1024, 256 * 8 * 8]);
     let c_inner = DeviceTensor::alloc_simple(device, vec![1024, 256 * 8 * 8]);
 
     let a_large = a_inner.clone();
-    let b_large = b_inner.repeat_unary(0, 1024).repeat_unary(1, 256 * 8 * 8);
+    // let a_large = a_inner.repeat_unary(0, 1024).repeat_unary(1, 256 * 8 * 8);
+    let b_large = b_inner.clone();
+    // let b_large = b_inner.repeat_unary(0, 1024).repeat_unary(1, 256 * 8 * 8);
     let c_large = c_inner.clone();
 
-    let iterations = 100;
+    println!("Autokernel");
+    let time_manual = profile_kernel(&stream, || launch(stream, &func, &a_large, &b_large, &c_large));
+    let tp_manual = (4 * a_large.shape().size()) as f32 / time_manual;
 
-    let start = stream.record_new_event();
-    for _ in 0..iterations {
-        launch(stream, &func, &a_large, &b_large, &c_large);
-        // launch_foo(stream, &a_large, &b_large);
-    }
-    let end = stream.record_new_event();
-    stream.synchronize();
-
-    let time_per_kernel = end.time_elapsed_since(&start) / iterations as f32;
-    let throughput = (4 * a_large.shape().size()) as f32 / time_per_kernel;
-
-    println!("Delta per kernel: {} ms", time_per_kernel * 1000.0);
-    println!("Kernels per second: {}", 1.0 / time_per_kernel);
+    println!("  Delta per kernel: {} ms", time_manual * 1000.0);
+    println!("  Kernels per second: {}", 1.0 / time_manual);
     println!(
-        "Manual throughput:\n  {} items/s\n  {} GBps",
-        throughput,
-        throughput / 1024f32.powi(3)
+        "  Item throughput:\n  {} items/s\n  {} GBps",
+        tp_manual,
+        tp_manual / 1024f32.powi(3)
+    );
+
+    // use inner tensors and hope that broadcasting works out
+    let args = TensorOpArgs::<DevicePtr> {
+        op_desc: TensorOpDescriptor::new(cudnnOpTensorOp_t::CUDNN_OP_TENSOR_ADD),
+        alpha_1: 1.0,
+        input_1_desc: a_inner.shape().descriptor(),
+        input_1_ptr: a_inner.ptr().clone(),
+        alpha_2: 1.0,
+        input_2_desc: b_inner.shape().descriptor(),
+        input_2_ptr: b_inner.ptr().clone(),
+        beta: 0.0,
+        output_desc: c_large.shape().descriptor(),
+        output_ptr: c_large.ptr().clone(),
+    };
+
+    println!("Cudnn TensorOp");
+    let time_cudnn = profile_kernel(stream, || args.run(&handle));
+    let tp_cudnn = (4 * a_large.shape().size()) as f32 / time_cudnn;
+    println!("  Delta per kernel: {} ms", time_cudnn * 1000.0);
+    println!("  Kernels per second: {}", 1.0 / time_cudnn);
+    println!(
+        "  Item throughput:\n  {} items/s\n  {} GBps",
+        tp_cudnn,
+        tp_cudnn / 1024f32.powi(3)
     );
 }
