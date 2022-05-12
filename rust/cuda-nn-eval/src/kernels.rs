7ab87d5b7ed7e473e947a592880d59fa5b2e89f8
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-05-12 17:16:55 +0200

    Remove stridedCopy experiment, it's been fully superseded by the new scalar autokernel.

--- rust/cuda-nn-eval/src/kernels.rs
+++ rust/cuda-nn-eval/src/kernels.rs
@@ -7,17 +7,6 @@ use cuda_sys::bindings::{cudaError, cudaStream_t};
 extern "C" {
     pub fn vectorAdd_main();
 
-    pub fn stridedCopyFloat(
-        stream: cudaStream_t,
-        rank: i32,
-        output_size: i32,
-        input_strides: *const i32,
-        output_strides: *const i32,
-        dense_strides: *const i32,
-        input: *const f32,
-        output: *mut f32,
-    ) -> cudaError;
-
     pub fn gatherFloat(
         stream: cudaStream_t,
         size: i32,
