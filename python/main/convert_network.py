fefe5a10e3339861428e29c7cb08be2df3123c39
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-06-03 13:39:26 +0200

    Add utility to convert from pt to onnx files.

---
+++ python/main/convert_network.py
@@ -0,0 +1,47 @@
+import os.path
+from argparse import ArgumentParser
+from typing import Optional
+
+import torch
+
+from lib.games import Game
+from lib.save_onnx import save_onnx
+
+
+def main():
+    parser = ArgumentParser()
+
+    parser.add_argument("--game", type=str, required=True)
+    parser.add_argument("--check_batch_size", type=int, default=None)
+    parser.add_argument("--force", action="store_true")
+    parser.add_argument("input", type=str)
+    parser.add_argument("output", type=str)
+
+    args = parser.parse_args()
+
+    game: str = args.game
+    check_batch_size: Optional[int] = args.check_batch_size
+    force: bool = args.force
+    input_path: str = args.input
+    output_path: str = args.output
+
+    assert input_path.endswith(".pt"), f"Input must be a .pt file"
+    assert output_path.endswith(".onnx"), f"Output must be an .onnx file"
+
+    assert os.path.exists(input_path), "Input path does not exist"
+
+    if os.path.exists(output_path):
+        if not force and os.path.getmtime(output_path) > os.path.getmtime(input_path):
+            print("Not converting network since .onnx output is newer than .pt input")
+            return
+        else:
+            print("Overwriting existing .onnx output")
+            os.remove(output_path)
+
+    game: Game = Game.find(game)
+    network = torch.jit.load(input_path)
+    save_onnx(game, output_path, network, check_batch_size)
+
+
+if __name__ == '__main__':
+    main()
