b58d26156d037e96f90a6961665b174a57c8e252
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-06-19 15:58:28 +0200

    Update to slightly changed Coord representation in board-game.

--- rust/kz-core/src/mapping/ttt.rs
+++ rust/kz-core/src/mapping/ttt.rs
@@ -1,5 +1,6 @@
 use board_game::board::Board;
-use board_game::games::ttt::{Coord, TTTBoard};
+use board_game::games::ttt::TTTBoard;
+use board_game::util::coord::Coord3;
 
 use crate::mapping::bit_buffer::BitBuffer;
 use crate::mapping::{InputMapper, MuZeroMapper, PolicyMapper};
@@ -17,8 +18,8 @@ impl InputMapper<TTTBoard> for TTTStdMapper {
     }
 
     fn encode_input(&self, bools: &mut BitBuffer, _: &mut Vec<f32>, board: &TTTBoard) {
-        bools.extend(Coord::all().map(|c| board.tile(c) == Some(board.next_player())));
-        bools.extend(Coord::all().map(|c| board.tile(c) == Some(board.next_player().other())));
+        bools.extend(Coord3::all().map(|c| board.tile(c) == Some(board.next_player())));
+        bools.extend(Coord3::all().map(|c| board.tile(c) == Some(board.next_player().other())));
     }
 }
 
@@ -27,12 +28,13 @@ impl PolicyMapper<TTTBoard> for TTTStdMapper {
         &[1, 3, 3]
     }
 
-    fn move_to_index(&self, _: &TTTBoard, mv: Coord) -> Option<usize> {
-        Some(mv.i())
+    fn move_to_index(&self, _: &TTTBoard, mv: Coord3) -> Option<usize> {
+        Some(mv.index() as usize)
     }
 
-    fn index_to_move(&self, _: &TTTBoard, index: usize) -> Option<Coord> {
-        Some(Coord::from_i(index))
+    fn index_to_move(&self, _: &TTTBoard, index: usize) -> Option<Coord3> {
+        assert!(index < 9);
+        Some(Coord3::from_index(index as u8))
     }
 }
 
@@ -46,7 +48,8 @@ impl MuZeroMapper<TTTBoard> for TTTStdMapper {
     }
 
     fn encode_mv(&self, result: &mut Vec<f32>, mv_index: usize) {
-        let mv = Coord::from_i(mv_index);
-        result.extend(Coord::all().map(|c| (c == mv) as u8 as f32))
+        assert!(mv_index < 9);
+        let mv = Coord3::from_index(mv_index as u8);
+        result.extend(Coord3::all().map(|c| (c == mv) as u8 as f32))
     }
 }
