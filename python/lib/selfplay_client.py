efc6774e0a442b64976226a5c27a7fbd30ae9cae
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-08-31 15:36:34 +0200

    Add new temperature settings to loop config.

--- python/lib/selfplay_client.py
+++ python/lib/selfplay_client.py
@@ -56,7 +56,8 @@ class SelfplaySettings:
     zero_temp_move_count: int
     dirichlet_alpha: float
     dirichlet_eps: float
-    search_policy_temperature: float
+    search_child_policy_temperature: float
+    search_root_policy_temperature: float
     full_search_prob: float
     full_iterations: int
     part_iterations: int
