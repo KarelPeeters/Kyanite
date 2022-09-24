5913f8f8479c5fc1277cbb0d687248ab93690a22
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-09-24 14:26:06 +0200

    Add additional scalar fusion test (that does not yet fuse properly).

--- rust/cuda-nn-eval/tests/root/scalar_fusion.rs
+++ rust/cuda-nn-eval/tests/root/scalar_fusion.rs
@@ -63,3 +63,23 @@ fn rejoining_scalar() {
     let input_tensor = rng_tensor((2, 3), &mut rng);
     test_all(&graph, 0, &[input_tensor], None)
 }
+
+#[test]
+fn inner_scalar_used() {
+    let mut graph = Graph::new();
+    let mut rng = StdRng::seed_from_u64(0);
+
+    let input = graph.input(shape![2, 3]);
+
+    let w1 = graph.constant(shape![1, 3], rng_vec(3, &mut rng));
+    let w2 = graph.constant(shape![2, 1], rng_vec(2, &mut rng));
+
+    let y1 = graph.mul(input, w1);
+    let y2 = graph.add(input, w2);
+    let y3 = graph.binary(BinaryOp::Max, y1, y2);
+
+    graph.output_all(&[y1, y3]);
+
+    let input_tensor = rng_tensor((2, 3), &mut rng);
+    test_all(&graph, 0, &[input_tensor], None)
+}
