7d428978055747efb2943e69a2df544632814559
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-12-06 23:12:58 +0100

    Write ataxx symmetry mappings to json for use in python data augmentation.

--- rust/kz-misc/src/bin/write_ataxx_mapping.rs
+++ rust/kz-misc/src/bin/write_ataxx_mapping.rs
@@ -1,10 +1,13 @@
 use std::fs::File;
 use std::io::Write;
 
-use board_game::board::BoardMoves;
+use board_game::board::{BoardMoves, BoardSymmetry};
 use board_game::games::ataxx::AtaxxBoard;
+use board_game::symmetry::{D4Symmetry, Symmetry};
 use board_game::util::coord::Coord8;
 use internal_iterator::InternalIterator;
+use itertools::{enumerate, Itertools};
+use serde;
 
 use kz_core::mapping::ataxx::AtaxxStdMapper;
 use kz_core::mapping::PolicyMapper;
@@ -14,6 +17,9 @@ fn main() -> std::io::Result<()> {
 
     let mut file_valid = File::create("ignored/mapping/ataxx_valid.txt")?;
     let mut file_encode = File::create("ignored/mapping/ataxx_index_to_move_input.txt")?;
+    let mut file_syms = File::create("ignored/mapping/ataxx_symmetry.json")?;
+
+    let mut all_syms = vec![];
 
     for size in 2..=8 {
         let mapper = AtaxxStdMapper::new(size);
@@ -22,7 +28,7 @@ fn main() -> std::io::Result<()> {
             .filter(|mv| mv.valid_for_size(size))
             .collect();
 
-        for (i, mv) in moves.into_iter().enumerate() {
+        for (i, &mv) in enumerate(&moves) {
             if i != 0 {
                 write!(&mut file_valid, ", ")?;
             }
@@ -47,7 +53,48 @@ fn main() -> std::io::Result<()> {
 
         writeln!(&mut file_valid)?;
         writeln!(&mut file_encode, "=")?;
+
+        let syms = D4Symmetry::all()
+            .iter()
+            .map(|&sym| {
+                let mut map_mv = vec![];
+
+                for &mv in &moves {
+                    let old_index = mapper.move_to_index(mv);
+                    let new_mv = AtaxxBoard::empty(size).map_move(sym, mv);
+                    let new_index = mapper.move_to_index(new_mv);
+
+                    if old_index >= map_mv.len() {
+                        map_mv.resize(old_index + 1, -1);
+                    }
+
+                    assert_eq!(map_mv[old_index], -1);
+                    map_mv[old_index] = new_index as isize;
+                }
+
+                all_numbers += map_mv.len();
+
+                AtaxxSymmetry {
+                    transpose: sym.transpose,
+                    flip_x: sym.flip_x,
+                    flip_y: sym.flip_y,
+                    map_mv,
+                }
+            })
+            .collect_vec();
+        all_syms.push(syms);
     }
 
+    serde_json::to_writer(&mut file_syms, &all_syms)?;
+
     Ok(())
+}
+
+#[derive(serde::Serialize)]
+struct AtaxxSymmetry {
+    pub transpose: bool,
+    pub flip_x: bool,
+    pub flip_y: bool,
+
+    pub map_mv: Vec<isize>,
 }
