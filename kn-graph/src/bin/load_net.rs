f67dfc06b6a7ed897041e21df88ec6a888cfa5fb
Karel Peeters <karel.peeters.leuven@gmail.com>
2024-03-10 00:15:12 +0100

    Expand load_net binary with svg output and optimization flag.

--- kn-graph/src/bin/load_net.rs
+++ kn-graph/src/bin/load_net.rs
@@ -2,17 +2,36 @@ use std::process::exit;
 
 use itertools::Itertools;
 
+use kn_graph::dot::graph_to_svg;
 use kn_graph::onnx::load_graph_from_onnx_path;
+use kn_graph::optimizer::{optimize_graph, OptimizerSettings};
 
 fn main() {
+    // parse args
     let args = std::env::args().collect_vec();
-    if args.len() != 2 {
-        eprintln!("Usage: load_net <path.onnx>");
+
+    let optimize = args.iter().any(|a| a == "-o");
+    let args = args.iter().filter(|&a| a != "-o").collect_vec();
+
+    if args.len() != 2 && args.len() != 3 {
+        eprintln!("Usage: load_net [-o] <path.onnx> [path.svg]");
         exit(1);
     }
-
     let path = &args[1];
-    let graph = load_graph_from_onnx_path(path, true).unwrap();
+    let out_path = args.get(2);
 
+    // load graph
+    let mut graph = load_graph_from_onnx_path(path, true).unwrap();
+
+    // optimize graph
+    if optimize {
+        graph = optimize_graph(&graph, OptimizerSettings::default());
+    }
+
+    // outputs
     println!("{}", graph);
+
+    if let Some(out_path) = out_path {
+        graph_to_svg(out_path, &graph, false, true).unwrap();
+    }
 }
